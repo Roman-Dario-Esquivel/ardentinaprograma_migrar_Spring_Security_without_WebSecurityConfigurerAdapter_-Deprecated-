4259e3dddb8033764e6f7d424a6e0e3517a3eb84
Roman-Dario-Esquivel <Roman-Dario-Esquivel>
2023-02-27 19:37:35 -0300

    compartir migracion de deprecad

---
+++ Entity/Rol.java
@@ -0,0 +1,45 @@
+package com.portfolio.Roman_Dario_Esquivel.Security.Entity;
+
+import com.portfolio.Roman_Dario_Esquivel.Security.Enums.RolNombre;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+
+@Entity
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RolNombre rolNombre;
+    // constuctor
+
+    public Rol() {
+    }
+    //setter y getter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public RolNombre getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+    
+    
+}
---
+++ MainSecurity.java
@@ -0,0 +1,76 @@
+package com.portfolio.Roman_Dario_Esquivel.Security;
+
+import com.portfolio.Roman_Dario_Esquivel.Security.Service.UserDetailsImpl;
+import com.portfolio.Roman_Dario_Esquivel.Security.jwt.JwtEntryPoint;
+import com.portfolio.Roman_Dario_Esquivel.Security.jwt.JwtTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class MainSecurity {
+
+    @Autowired
+    UserDetailsImpl userDetailsService;
+
+    @Autowired
+    JwtEntryPoint jwtEntryPoint;
+    
+   
+    @Bean
+    public JwtTokenFilter jwtTokenFilter() {
+        return new JwtTokenFilter();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeHttpRequests()
+     
+                .requestMatchers("**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.authenticationProvider(authenticationProvider());
+        
+        return http.build();
+    }
+
+  
+  @Bean
+  public DaoAuthenticationProvider authenticationProvider() {
+      DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+       
+      authProvider.setUserDetailsService(userDetailsService);
+      authProvider.setPasswordEncoder(passwordEncoder());
+   
+      return authProvider;
+  }
+
+  
+  /*
+    @Bean
+    public WebSecurityCustomizer  webSecurityCustomizer() {
+        return (web) ->web.ignoring().requestMatchers();
+    }*/
+
+}
